cc3e774a29445a6f22fd4890d71735c9d0393a4e
Carlos Pirela <pirela_carlos@hotmail.com>
2020-11-17 13:03:09 -0400

    Initial setup

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const data = require('./data/gothedistance.json');
+
+app.listen(port, () => {
+  console.log('====================================');
+  console.log(`Server up, listening on port ${port}`);
+  console.log('====================================');
+});
+
+app.get('/', function(req, res) {
+  res.send('Hello World!');
+});
+
+app.get('/goTheDistance', function(req, res) {
+  res.send(JSON.stringify(data));
+});
